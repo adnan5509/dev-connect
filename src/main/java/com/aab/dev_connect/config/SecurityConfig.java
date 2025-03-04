4644bdf07e2136f8f9fec0dd734878e9948549ba
adnan5509 <adnanafzal5509@gmail.com>
2025-03-04 17:29:40 +0100

    Spring Security Config - 1

--- src/main/java/com/aab/dev_connect/config/SecurityConfig.java
+++ src/main/java/com/aab/dev_connect/config/SecurityConfig.java
@@ -1,20 +1,45 @@
 package com.aab.dev_connect.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
-@EnableWebSecurity
+@Configuration
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
-//        http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());
-        return http.build();
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails projectOwner = User.withDefaultPasswordEncoder()
+                .username("adnan")
+                .password("adnan")
+                .roles("PROJECT_OWNER")
+                .build();
+
+        UserDetails taskOwner = User.withDefaultPasswordEncoder()
+                .username("kamran")
+                .password("kamran")
+                .roles("TASK_OWNER")
+                .build();
+
+        return new InMemoryUserDetailsManager(projectOwner, taskOwner);
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/projects").hasRole("PROJECT_OWNER") // Only ADMIN can create projects
+                        .requestMatchers("/api/projects/*").hasRole("PROJECT_OWNER") // Only ADMIN can view projects
+                        .requestMatchers("/api/tasks").hasAnyRole("TASK_OWNER","PROJECT_OWNER") // Only USER can create tasks
+                        .requestMatchers("/api/tasks/*").hasRole("TASK_OWNER") // Only USER can view tasks
+                        .anyRequest().authenticated())
+                .formLogin(login -> login.permitAll())
+                .logout(logout -> logout.permitAll());
+        return httpSecurity.build();
+    }
+
+
 }
