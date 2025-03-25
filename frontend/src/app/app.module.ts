6d0b8b5c9681b9bb5a3aca22f44aaa733550a937
adnan5509 <adnanafzal5509@gmail.com>
2025-03-25 15:09:57 +0100

    Setting up Frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProjectListComponent } from './project-list/project-list.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { ProjectDetailsCardComponent } from './project-details-card/project-details-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectListComponent,
+    ProjectDetailsCardComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/project-list/project-list.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { HttpClient } from '@angular/common/http';
+import { Project } from '../model/project';
+
+
+@Component({
+  selector: 'app-project-list',
+  templateUrl: './project-list.component.html',
+  styleUrls: ['./project-list.component.css'],
+  imports: [MatCardModule, MatChipsModule, MatProgressBarModule],
+})
+export class ProjectListComponent {
+
+  Projects: Project[] = [];
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.http.get<Project[]>('http://localhost:8080//api/projects/user/1').subscribe((data) => {
+      this.Projects = data;
+    });
+  }
+}
